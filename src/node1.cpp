6fcb210a8d10c56e33b3a92bba9af5b942e0552a
rizemmahendra <ryzemmahendra@gmail.com>
2023-11-03 23:31:04 +0700

    menambahkan kodingan lora

--- src/node1.cpp
+++ src/node1.cpp
@@ -1,4 +1,18 @@
 #include <Arduino.h>
+// ===================== LoRa Library & Config =====================
+#include <SPI.h>
+#include <LoRa.h>
+
+const long frequency = 433E6; // LoRa Frequency
+const int nssPin = 10;        // LoRa radio chip select
+const int resetPin = 9;       // LoRa radio reset
+const int dio0Pin = 2;        // change for your board; must be a hardware interrupt pin
+String data;
+byte localAddress = 0x01; // address this device
+byte destination = 0x02;  // address destination
+unsigned long lastSendTime = 0;
+unsigned int interval = 2000;
+// =================================================================
 
 // ================== Ultrasonik Library & Config ==================
 const int trigPin = 4;
@@ -18,15 +32,46 @@ float ultrasonik()
     return jarak;
 }
 
+void setupLora()
+{
+    LoRa.setPins(nssPin, resetPin, dio0Pin);
+    if (!LoRa.begin(frequency))
+    {
+        Serial.println("Lora init failed. Check your connections");
+        while (true)
+            ;
+    }
+    Serial.println("Lora init Succeeded");
+}
+
+void sendMessage(String message)
+{
+    LoRa.beginPacket();
+    LoRa.write(destination);
+    LoRa.write(localAddress);
+    LoRa.write(message.length());
+    LoRa.print(message);
+    LoRa.endPacket();
+}
+
 void setup()
 {
     Serial.begin(9600);
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
+    setupLora();
 }
 void loop()
 {
-    Serial.print("jarak : ");
-    Serial.println(ultrasonik());
+    float jarak = ultrasonik();
+    data = (String)jarak;
+
     delay(1000);
+    if (millis() - lastSendTime > interval)
+    {
+        // String message = (String)jarak;
+        sendMessage(data);
+        Serial.println("mengirim data : " + data);
+        lastSendTime = millis();
+    }
 }
