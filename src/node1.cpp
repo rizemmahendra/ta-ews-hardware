f1e2ec5e396530bf3ee8816821e176920129febf
rizemmahendra <ryzemmahendra@gmail.com>
2024-07-15 12:15:22 +0700

    feat(node1): add implementation for rain gauge sensor

--- src/node1.cpp
+++ src/node1.cpp
@@ -7,8 +7,8 @@ const byte resetPin = 9;        // LoRa radio reset
 const byte dio0Pin = 2;         // change for your board; must be a hardware interrupt pin
 const byte localAddress = 0x01; // address this device
 const byte destination = 0x02;  // address destination
-unsigned long lastSendTime = 0;
-unsigned int interval = 500;
+uint64_t lastSendTime = 0;
+uint16_t interval = 5000; // 5000ms or 5s
 
 MyLora *myLora = new MyLora(&nssPin, &resetPin, &dio0Pin, &localAddress);
 String message;
@@ -25,6 +25,7 @@ const byte ldrPin = A0;
 DataSensor *ldr = new DataSensor();
 // ================== Rain Gauge ==================
 const byte reedSwitchPin = 3;
+const float tickVolume = 1.4;
 DataSensor *reedSwitch = new DataSensor();
 // ================================================
 
@@ -35,13 +36,25 @@ MySensor *mySensor = new MySensor();
 #include <ArduinoJson.h>
 // =================================================================
 
+void handleReedIntterupt()
+{
+    static unsigned long prevReed = 0;
+    if (millis() - prevReed >= 50) // mencengah double increment akibat bouncing
+    {
+        Serial.println(prevReed);
+        mySensor->tickIncreament();
+        Serial.println(F("Increment Tick"));
+        prevReed = millis();
+    }
+}
+
 void setup()
 {
     Serial.begin(9600);
 
     mySensor->initiliazeWaterLevel(trigPin, echoPin);
     mySensor->initiliazeTurbdidity(ldrPin);
-    mySensor->initiliazeRainGauge(reedSwitchPin);
+    mySensor->initiliazeRainGauge(reedSwitchPin, tickVolume, handleReedIntterupt);
 
     myLora->initilize(frequency);
 }
@@ -51,13 +64,18 @@ void loop()
     mySensor->getValueWaterLevel(ultrasonik);
     mySensor->getValueTurbdity(ldr);
     mySensor->getValueRainGauge(reedSwitch);
-    Serial.println(ultrasonik->value);
 
-    static unsigned long current = 0;
-
-    if (millis() - current > 5000 || current == 0)
+    static uint64_t prevGetRainGauge = 0;
+    if (millis() - prevGetRainGauge >= 60000) // get every 1menit
     {
-        current = millis();
+        mySensor->resetTickCount();
+        prevGetRainGauge = millis();
+    }
+
+    static uint64_t prevSend = 0;
+    if (millis() - prevSend > interval || prevSend == 0)
+    {
+        prevSend = millis();
         StaticJsonDocument<128> data;
         data["t"] = ultrasonik->value;
         data["ts"] = ultrasonik->status;
@@ -68,12 +86,12 @@ void loop()
 
         serializeJson(data, message);
 
-        unsigned long prev2 = millis();
+        // unsigned long prev2 = millis();
         myLora->sendMessage(destination, message);
-        Serial.print("lama mengirimkan data : ");
-        Serial.println(millis() - prev2, DEC);
         Serial.println("mengirim data : " + message);
-        Serial.println("-----------------------------------------");
+        // Serial.print("lama mengirimkan data : ");
+        // Serial.println(millis() - prev2, DEC);
+        Serial.println(F("-----------------------------------------"));
         message = "";
     }
 }
