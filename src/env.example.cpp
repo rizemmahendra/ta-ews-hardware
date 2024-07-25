ca39af4024d988544dd998989cdd44e7384a6cdc
rizemmahendra <ryzemmahendra@gmail.com>
2024-07-25 13:30:00 +0700

    fix(env): change from .env to env.cpp
    there is a problem when import key value with escape character, so .env file is changed to env.cpp to resolve problem

---
+++ src/env.example.cpp
@@ -0,0 +1,23 @@
+#define NAME "Early Warning System"
+#define AUTHOR ""
+#define VERSION 1.0
+
+//  For Wifi Connection
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+//  For Connection to Firebase
+#define API_KEY ""
+#define USER_EMAIL ""
+#define USER_PASSWORD ""
+#define FIREBASE_PROJECT_ID ""
+
+// For Firebase Firestore and Firebase Messaging
+#define ID_SUNGAI ""
+#define CLIENT_EMAIL ""
+#define PRIVATE_KEY ""
+
+// For Syncronize Time
+#define NTP_SERVER "id.pool.ntp.org"
+#define NTP_GMT_OFFSET_SEC 25200
+#define NTP_DAYLIGHT_OFFSET_SEC 0
